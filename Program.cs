29ccdcde8d09d086e7f3e750d15f123a8d256691
kei19 <kitoyu22@gmail.com>
2025-05-16 16:53:17 +0700

    initial: answer checker palindrome

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CheckerPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================Aplikasi Checker Palindrome================");
+            Console.WriteLine("Ketik 'exit' untuk keluar. \n");
+
+            while (true)
+            {
+                Console.WriteLine("Masukkan kalimat: ");
+                string? input = Console.ReadLine();
+
+                if (string.Equals(input, "exit", StringComparison.OrdinalIgnoreCase)) {
+                    Console.WriteLine("Aplikasi dihentikan");
+                    break;
+                }
+
+                bool output = IsPalindrome(input);
+                Console.WriteLine($"Apakah palindrom ?{output}");
+            }
+        }
+
+        static bool IsPalindrome(string? word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+                return false;
+
+            word = word.ToLower().Replace(" ", "");
+
+            int left = 0;
+            int right = word.Length - 1;
+
+            while (left < right)
+            {
+                if (word[left] != word[right])
+                    return false;
+
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
